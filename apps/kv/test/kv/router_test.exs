0930747d7874497fe2b865b238f29adb6a850777
digitsu <digitsu@gmail.com>
2021-12-19 14:05:02 +0900

    Completed releases and nodes

---
+++ apps/kv/test/kv/router_test.exs
@@ -0,0 +1,27 @@
+defmodule KV.RouterTest do
+  @moduledoc false
+  use ExUnit.Case
+
+  setup_all do
+    current = Application.get_env(:kv, :routing_table)
+
+    Application.put_env(:kv, :routing_table, [
+      {?a..?m, :"foo@arcteryx"},
+      {?n..?z, :"bar@arcteryx"}
+    ])
+
+    on_exit fn -> Application.put_env(:kv, :routing_table, current) end
+  end
+
+  @tag :distributed
+  test "route requests across nodes" do
+    assert KV.Router.route("hello", Kernel, :node, []) == :"foo@arcteryx"
+    assert KV.Router.route("world", Kernel, :node, []) == :"bar@arcteryx"
+  end
+
+  test "raises on unknown entries" do
+    assert_raise RuntimeError, ~r/could not find entry/, fn ->
+      KV.Router.route(<<0>>, Kernel, :node, [])
+    end
+  end
+end
